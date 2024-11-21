6277824193417b73f19831258a32816172a8e346
ma1abakiev <malabakievramzan@gmail.com>
2024-11-21 15:04:17 +0600

    add:yandex map

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,30 @@
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
+
+
 export const Home = () => {
-  return <div>Home</div>;
-};
+// Центр Бишкека
+const mapCenter = [42.874621, 74.612198];
+
+return (
+    <YMaps query={{apikey : '1edf294d-664d-4286-af6a-ac306969cf67'}}>
+        <div style={{ width: '100%', height: '100vh' }}>
+            <Map
+                defaultState={{
+                    center: mapCenter,
+                    zoom: 12,
+                }}
+                width="100%"
+                height="100%"
+            >
+                {/* Отметка в центре Бишкека */}
+                <Placemark 
+                    geometry={mapCenter}
+                    properties={{
+                        hintContent: 'Центр Бишкека',
+                        balloonContent: 'Площадь Ала-Тоо',
+                    }}
+                />
+            </Map>
+        </div>
+    </YMaps>
+);}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  important: '#root',
   theme: {
     extend: {
-      screens: {
-        'xs-max': { max: '320px' },
-        'sm-max': { max: '480px' },
-        'md-max': { max: '768px' },
-        'lg-max': { max: '1024px' },
-        'xl-max': { max: '1440px' },
-      },
       container: {
         center: true,
         padding: {
@@ -21,18 +13,8 @@ export default {
           'lg-max': '100px',
           'xl-max': '150px',
         },
-
-        screens: {
-          'xl-max': '1440px', // Specific width for xl-max screens
-        },
       },
     },
-    fontFamily: {
-      serif: ['Roboto'],
-      sans: ['Roboto'],
-    },
-    colors: {},
-    backgroundImage: {},
   },
   plugins: [],
 }
